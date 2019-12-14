f4b5f61f3e24ec543179643007555feea328a385
Gurkaran Singh <sgurkaran2000@gmail.com>
2019-12-14 12:48:00 +0530

    Add files via upload

--- models/Post.js
+++ models/Post.js
@@ -1,26 +1,43 @@
-const {model , Schema} = require('mongoose')
+// const {model , Schema} = require('mongoose')
 
-const postSchema = new Schema({
-    body: String,
-    username: String,
-    createdAt: String,
-    comments: [
-        {
-            body: String,
-            username: String,
-            createdAt: String,
-        },
-    ],
-    likes: [
-        {
-            username: String,
-            createdAt: String,
-        },
-    ],
-    user:{
-        type: Schema.Types.ObjectId,
-        ref: 'user'
-    }
+// const postSchema = new Schema({
+//     body: String,
+//     username: String,
+//     createdAt: String,
+//     comments: [
+//         {
+//             body: String,
+//             username: String,
+//             createdAt: String,
+//         },
+//     ],
+//     likes: [
+//         {
+//             username: String,
+//             createdAt: String,
+//         },
+//     ],
+//     user:{
+//         type: Schema.Types.ObjectId,
+//         ref: 'user'
+//     }
+// })
+
+// module.exports = model('Post', postSchema)
+  
+const Sequelize = require('sequelize');
+const db = require('../config/db');
+
+const Post = db.define('post', {
+    body: {
+        type: Sequelize.STRING
+    },
+    username: {
+        type: Sequelize.STRING
+    },
+    createdAt: {
+        type: Sequelize.STRING
+    },
 })
 
-module.exports = model('Post', postSchema)
+module.exports = Post;
